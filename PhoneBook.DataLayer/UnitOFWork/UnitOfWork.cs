0ce4a3ff7c9995d05608dcfd7b8a5275a1e02450
HamidKn <hamid.keshmiri@gmail.com>
2021-01-04 19:47:24 +0100

    Icons changed to English

--- PhoneBook.DataLayer/UnitOFWork/UnitOfWork.cs
+++ PhoneBook.DataLayer/UnitOFWork/UnitOfWork.cs
@@ -1,10 +1,6 @@
 ﻿using PhoneBook.DataLayer.Repository;
 using PhoneBook.DataLayer.Service;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhoneBook.DataLayer.UnitOFWork
 {
